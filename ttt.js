365dbbecd0440eb6b733e3384f5f6ea0244290f7
FocusGaurd-Dev <focusgaurd.dev@gmail.com>
2026-06-21 15:29:05 +0530

    scorecard introduced, boxes green no console msg

--- ttt.js
+++ ttt.js
@@ -1,8 +1,11 @@
 let boxes=document.querySelectorAll('.box');
 let resetBtn=document.getElementById('reset');
 let turno=true;
-let win=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+const win=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 let gameend=false;
+let show=document.getElementById('show');
+let player1=0;
+let player2=0;
 
 boxes.forEach((box) =>{
     box.addEventListener("click",() =>{
@@ -21,7 +24,17 @@ boxes.forEach((box) =>{
         
     })
 })
+let boxdisabled=() =>{
+    boxes.forEach((box) =>{
+        box.disabled=true;
+    })
+}
+let greenbox = (pattern) =>{
+    boxes[pattern[0]].style.backgroundColor="lightgreen";
+    boxes[pattern[1]].style.backgroundColor="lightgreen";
+    boxes[pattern[2]].style.backgroundColor="lightgreen";
 
+}
 let checkwinner=() => {
     for(let pattern of win){
         let pos1=boxes[pattern[0]].innerText;
@@ -30,11 +43,17 @@ let checkwinner=() => {
         if (pos1===pos2 && pos2===pos3){
             if(pos1 !="" && pos2 !="" && pos3 !=""){
                 if(pos1==="X"){
-                    console.log("player1 win");
+                    greenbox(pattern);
+                    boxdisabled();
+                    player1++;
+                    show.innerHTML=`Player 1 win<br> player 1 score: ${player1} <br> player 2 score: ${player2}`;
                     gameend=true;
                     return;
                 }else if(pos1==="O"){
-                    console.log("player2 win");
+                    greenbox(pattern);
+                    boxdisabled();
+                    player2++;
+                    show.innerHTML=`Player 2 win<br> player 1 score: ${player1} <br> player 2 score: ${player2}`;
                     gameend=true;
                     return;
                 }
@@ -53,7 +72,7 @@ let checkdraw=() => {
         }
     })
     if(is_draw){
-        console.log("draw");
+        show.innerText="It's a draw!";
     }
 }
 
@@ -61,8 +80,10 @@ resetBtn.addEventListener("click", () =>{
     boxes.forEach((box) => {
         box.innerText="";
         box.disabled=false;
+        box.style.backgroundColor="";
     })
     gameend=false;
     console.clear();
     turno=true;
+    show.innerText="";
 })
